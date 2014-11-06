175b4b128895385ae25843e3c07af6a7dbab0d0e
Oscar Mario Viquez <erueco87@gmail.com>
2014-11-05 19:32:03 -0600

    Law description section

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -42,4 +42,15 @@ angular
       .otherwise({
         redirectTo: '/'
       });
+  })
+  .run(function($rootScope, $location, $anchorScroll, $routeParams) {
+
+    //when the route is changed scroll to the proper element.
+    $rootScope.$on('$routeChangeSuccess', function(newRoute, oldRoute) {
+
+      $location.hash($routeParams.section);
+      $anchorScroll();
+
+    });
+
   });
