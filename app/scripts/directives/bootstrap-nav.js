ab38f7c8b633a3eb54d694503d016f38f15d45e5
Oscar Viquez <erueco87@gmail.com>
2014-11-08 20:21:55 -0600

    Fixes mobile nav not closing on item click

---
+++ app/scripts/directives/bootstrap-nav.js
@@ -0,0 +1,27 @@
+/*jshint unused:false*/
+/* global $:false */
+
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name informaticaLibreApp.directive:bootstrapNav
+ * @description
+ * # bootstrapNav
+ * Directive of the informaticaLibreApp bootstrap nav
+ */
+angular.module('informaticaLibreApp')
+  .directive('bootstrapNav', function () {
+    return {
+      restrict: 'A',
+      link: function (scope, element, attrs) {
+        
+        $(element).find('.navbar-nav li a').on('click', function() {
+            
+          $('.navbar-toggle').click();
+        
+        });
+
+      }
+    };
+  });
